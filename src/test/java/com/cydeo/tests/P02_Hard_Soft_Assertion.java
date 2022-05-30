c32855a45b126abe66a062922ae02cc63c49c7d6
alimD87 <101231678+alimD87@users.noreply.github.com>
2022-05-30 16:09:06 -0400

    Selenium_day6_TestNG

---
+++ src/test/java/com/cydeo/tests/P02_Hard_Soft_Assertion.java
@@ -0,0 +1,33 @@
+package com.cydeo.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import static org.testng.Assert.assertTrue;
+
+public class P02_Hard_Soft_Assertion {
+
+    @Test
+    public void hardAssertion(){
+
+        Assert.assertEquals("apple", "app");
+        System.out.println("After Assertion failed");
+        assertTrue("orange".equals("oranger"));
+        System.out.println("After Assertion failed");
+    }
+
+    @Test
+    public void softAssertion(){
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals("apple", "app");
+        System.out.println("After Assertion failed");
+        softAssert.assertTrue("orange".equals("oranger"));
+        System.out.println("After Assertion failed");
+
+        softAssert.assertAll();
+    }
+
+
+}
